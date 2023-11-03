1d1bcd9490d2c4cbfbc2ff46dde2c8f67b83eca8
jtk0817 <144420372+jtk0817@users.noreply.github.com>
2023-11-03 16:13:11 +0900

    Update 9주차 퀴즈,.py

--- 9주차 퀴즈,.py
+++ 9주차 퀴즈,.py
@@ -1,13 +1,14 @@
-height = int(input("크리스마스 트리의 높이를 설정하세요: "))
+트리높이 = int(input("크리스마스 트리의 높이를 설정하세요: "))
 
-for i in range(1, height + 1):
-    spaces = height - i
-    stars = 2 * i - 1
+for i in range(1, 트리높이 + 1):
+    트리크기 = 트리높이 - i
+    별 = 2 * i - 1
 
-    for j in range(spaces):
+    for j in range(트리크기):
         print(" ", end="")
 
-    for j in range(stars):
+    for j in range(별):
         print("*", end="")
 
     print()
+
